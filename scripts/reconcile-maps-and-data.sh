6aa494be6f9566044b15588c5997abf1013aefd8
Bill Abresch <bill.abresch@gmail.com>
2013-05-07 20:25:25 -0400

    took care of the congos and gambia

--- scripts/reconcile-maps-and-data.sh
+++ scripts/reconcile-maps-and-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mv ../maps/Congo.json ../maps/Democratic_Republic_of_the_Congo.json
 mv ../maps/Republic_of_the_Congo.json ../maps/Republic_of_Congo.json
-cp ../maps/Senegal.json ../maps/Gambia.json
+cp -f ../maps/Senegal.json ../maps/Gambia.json
